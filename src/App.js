8849643d86615f57a3d4e2058b9e9c9e8ee61c92
thesagarmahajan <sagarmahajan31997@gmail.com>
2022-10-26 08:23:00 +0530

    Relative Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-// import SimpleCounter from './hooks/use_state/SimpleCounter';
-// import TicTacToe from './hooks/use_effect/TicTacToe';
-// import DataBinding from './hooks/use_state/DataBinding';
-import EmailValidator from './hooks/use_effect/EmailValidator';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import LifecycleMethods from './components_types/LifecycleMethods';
 
 function App() {
    return (
-    <EmailValidator />
+    <LifecycleMethods />
   );
 }
 
