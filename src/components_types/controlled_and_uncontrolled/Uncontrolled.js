87e66db47641355284da44a8389e282952340088
thesagarmahajan <sagarmahajan31997@gmail.com>
2022-10-31 20:43:23 +0530

    Uncontrolled Component Demo

---
+++ src/components_types/controlled_and_uncontrolled/Uncontrolled.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Uncontrolled(){
+
+    let name = React.createRef()
+    let phone = React.createRef()
+    
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(name.current.value)
+        console.log(phone.current.value)
+    }
+    return (<>
+        <h1>This is Uncontrolled</h1>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" ref={name} />
+            <input type="text" placeholder="phone" ref={phone} />
+
+            <button>Submit</button>
+        </form>
+    </>)
+}
+
+export default Uncontrolled;
